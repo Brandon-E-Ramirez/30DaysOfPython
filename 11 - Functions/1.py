a66874e11cebdaa5f52c8efab995ea74b2900112
Brandon-E-Ramirez <brandon.evaristo.ramirez@gmail.com>
2023-08-05 15:12:09 -0500

    finished functions

--- 11 - Functions/1.py
+++ 11 - Functions/1.py
@@ -1,11 +1,7 @@
+import cmath;
 """
 Exercises: Level 1
 
-    #Write a function called check-season, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
-    #Write a function called calculate_slope which return the slope of a linear equation
-    #Quadratic equation is calculated as follows: ax² + bx + c = 0. Write a function which calculates solution set of a quadratic equation, solve_quadratic_eqn.
-    #Declare a function named print_list. It takes a list as a parameter and it prints out each element of the list.
-    #Declare a function named reverse_list. It takes an array as a parameter and it returns the reverse of the array (use loops).
 """
 
     #Declare a function add_two_numbers. It takes two parameters and it returns a sum.
@@ -36,6 +32,71 @@ def CelciusToFahrenheit (Celcius):
     Fahrenheit=((9/5)*Celcius)+32;
     return Fahrenheit;
 print(f"{inputTemp} degrees Celcius is {CelciusToFahrenheit(inputTemp)} degrees in Fahrenheit\n")
+    #Write a function called check-season, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
+seasons = ["January","February","March","April","May","June","July","August","September","October","Novemeber","December"];
+def season (month):
+    if month =='January' or month == 'February' or month == 'March':
+        return "Spring";
+    elif month == 'April' or month=='May'or month== 'June':
+        return "Summer";
+    elif month == 'July'or month=='August' or month== 'September':
+        return "Autumn";
+    elif month == 'October' or month=='November' or month== 'Decemberr':
+        return "Winter";
+    else: 
+        print("Invalid Month!");
+
+print(f"The month {seasons[5]} is in the season of {season(seasons[5])}\n");
+
+    #Quadratic equation is calculated as follows: ax² + bx + c = 0. Write a function which calculates solution set of a quadratic equation, solve_quadratic_eqn.
+def solve_quadratic_eqn(a,b,c):
+    #solve ax^2 + bx +c = 0
+    ans = "";
+    ansPlus= ((-b) + cmath.sqrt((b**2)-(4*a*c)))/2*(a);
+    ansMinus= ((-b) - cmath.sqrt((b**2)-(4*a*c)))/2*(a);
+    ans = str(ansPlus) + ", " + str(ansMinus);
+    return ans;
+
+x=3;
+y=4;
+z=2;
+
+res = solve_quadratic_eqn(x,y,z);
+print(f"The quadratic equations solutions \"x\" (in the expression \"ax^2 + bx + c 0\") for a={x}, b={y}, & c={z} is: {res}\n")
+
+    #Write a function called calculate_slope which return the slope of a linear equation
+    #y = mx +b
+    #Standard form Ax + By = C
+
+#first vertex (a,b), second is (c,d) (in the form "(x,y)")
+#use formula (d-b)/(c-a) or "rise over run"
+def calculate_slope(a,b,c,d):
+        slope = (d-b)/(c-a);
+        return slope;
+a = 2
+b = 6
+c = 5
+d = 10
+print(f"The slope of a={a}, b={b}, c={c}, & d={d} is {calculate_slope(a,b,c,d)}")
+
+    #Declare a function named print_list. It takes a list as a parameter and it prints out each element of the list.
+def print_list(list):
+    for i in list:
+        print(i,end=" ")
+    print("\n")
+
+arr = ["Texas","Alabama", "New York", "Utah", "Washington"]
+print_list(arr);
+    #Declare a function named reverse_list. It takes an array as a parameter and it returns the reverse of the array (use loops).
+def reverse_list(arr):
+        for i in range(len(arr) - 1, -1, -1):
+            print(arr[i], end=" ");
+
+reverse_list(arr);
+
+
+
+
 """
 print(reverse_list([1, 2, 3, 4, 5]))
 # [5, 4, 3, 2, 1]
