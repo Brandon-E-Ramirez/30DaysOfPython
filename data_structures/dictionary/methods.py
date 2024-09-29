71602302e76916504073610c2a001cefa21d8f3f
Brandon-E-Ramirez <brandon.evaristo.ramirez@gmail.com>
2024-09-29 14:28:29 -0500

    dictionary functions

---
+++ data_structures/dictionary/methods.py
@@ -0,0 +1,17 @@
+#our dictionary
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = car.copy()
+print(x) 
+
+
+
+
+
+'''
+car.clear() #remove all elements from dictioanary
+print(car) 
+'''
